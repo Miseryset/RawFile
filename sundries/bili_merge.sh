bfef9133cde8d20663401bd55e4915d173d80b8f
Miseryset <48995210+Miseryset@users.noreply.github.com>
2023-01-30 20:05:41 +0800

    Add files via upload

---
+++ sundries/bili_merge.sh
@@ -0,0 +1,27 @@
+
+export PATH=${PATH}:"/data/adb/ffmpeg"
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:"/data/adb/ffmpeg/libs"
+
+#bili_down="/sdcard/Android/data/tv.danmaku.bili/download"
+#output_dir="/sdcard/Document"
+bili_down="${_bili_down}"
+output_dir="${_output_dir}"
+
+for scan in `ls ${bili_down}` ; do
+  json=`find "${bili_down}/${scan}" -type f | grep "entry.json"`
+  title=`jq '.title' "${json}" | tr -d \"`
+  owner_name=`jq '.owner_name' "${json}" | tr -d \"`
+  
+  if [ -f "${output_dir}/${owner_name}_${title}_${scan}.mp4" ] ; then
+    echo "${output_dir}/${owner_name}_${title}_${scan}.mp4 已存在，跳过"
+  else
+    mkdir -p "${output_dir}/tmp"
+    for tt in "audio" "video" ; do
+      ff=`find "${bili_down}/${scan}" -type f | grep "${tt}.m4s"`
+      cpg -rg "${ff}" "${output_dir}/tmp"
+    done
+    ffmpeg -i "${output_dir}/tmp/video.m4s" -i "${output_dir}/tmp/audio.m4s" -vcodec copy -acodec copy "${output_dir}/${owner_name}_${title}_${scan}.mp4"
+    rm -r "${output_dir}/tmp"
+  fi
+  
+done
